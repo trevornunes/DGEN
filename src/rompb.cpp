3958f44ee5df04bc925d1f5cd83b958331ae939d
Trevor Nunes <trevor.nunes@gmail.com>
2012-11-15 01:10:08 -0500

    updated comments

---
+++ src/rompb.cpp
@@ -0,0 +1,376 @@
+/*
+ * Simple rom path retrieval and file/path checker. Used by emulators to simplify the repetitive logic
+ *
+ */
+
+
+#include "rompb.h"
+#include <dirent.h>
+#include <iostream>
+#include <fstream>
+
+
+static const char *romPath_xxx = "/accounts/1000/shared/misc/roms";
+static const char *romPath_nes = "/accounts/1000/shared/misc/roms/nes";
+static const char *romPath_gba = "/accounts/1000/shared/misc/roms/gba";
+static const char *romPath_pce = "/accounts/1000/shared/misc/roms/pce";
+static const char *romPath_smd = "/accounts/1000/shared/misc/roms/smd";
+static const char *romPath_gb  = "/accounts/1000/shared/misc/roms/gb";
+
+#define DEBUG 1
+
+//*********************************************************
+//
+//*********************************************************
+Rompb::Rompb(rom_type_t rtype)
+{
+  activeRom_m = "";
+  activeRomPath_m = "";
+  activeRomList_vsm.clear();
+  romType_m = rtype;
+  activeRomIndex_m =0;
+
+  switch(romType_m)
+  {
+    case rom_nes_c:
+    	 activeRomPath_m = romPath_nes;
+    	 extensions_vsm.push_back("nes");
+    	 extensions_vsm.push_back("NES");
+         break;
+
+    case rom_gb_c:
+    case rom_gbc_c:
+    	 activeRomPath_m = romPath_gb;
+    	 extensions_vsm.push_back("gb");
+    	 extensions_vsm.push_back("gbc");
+    	 extensions_vsm.push_back("bin");
+    	 extensions_vsm.push_back("zip");
+    	break;
+
+    case rom_gba_c:
+    	 activeRomPath_m = romPath_gba;
+    	 extensions_vsm.push_back("gba");
+    	 extensions_vsm.push_back("GBA");
+    	 extensions_vsm.push_back("zip");
+    	 extensions_vsm.push_back("bin");
+         break;
+
+    case rom_pce_c:
+    	 activeRomPath_m = romPath_pce;
+    	 extensions_vsm.push_back("pce");
+    	 break;
+
+    case rom_smd_c:
+    	 activeRomPath_m = romPath_smd;
+    	 extensions_vsm.push_back("smd");
+    	 extensions_vsm.push_back("gen");
+    	 extensions_vsm.push_back("bin");
+    	 break;
+
+    default:  activeRomPath_m = romPath_xxx;
+              break;
+  }
+
+  cfgFilePath_m = activeRomPath_m + "pbrom.cfg";
+#ifdef DEBUG
+  cout << "cfgFilePath_m " << cfgFilePath_m;
+#endif
+  loadState();
+}
+
+
+bool Rompb::extensionIsValid(string ext)
+{
+  unsigned int i = 0;
+  for(i =0; i < extensions_vsm.size(); i++)
+  {
+	 if( extensions_vsm[i] == ext)
+	 {
+		 return true;
+	 }
+  }
+
+  return false;
+}
+
+
+bool Rompb::isADir(string dpath)
+{
+  struct stat sb;
+
+  if (stat( dpath.c_str(), &sb) == -1)
+  {
+	perror("stat");
+	return false;
+  }
+
+  switch (sb.st_mode & S_IFMT)
+  {
+	    case S_IFBLK:  printf("block device\n");
+	                   break;
+
+	    case S_IFCHR:  printf("character device\n");
+	                   break;
+
+	    case S_IFDIR:  printf("directory\n");
+	                   return true;
+	                   break;
+
+	    case S_IFIFO:  printf("FIFO/pipe\n");               break;
+	    case S_IFLNK:  printf("symlink\n");                 break;
+	    case S_IFREG:  printf("regular file\n");            break;
+	    case S_IFSOCK: printf("socket\n");                  break;
+	    default:       printf("unknown?\n");                break;
+  }
+
+  return false;
+}
+
+//*********************************************************
+//
+//*********************************************************
+bool Rompb::pathExists(string dpath)
+{
+	DIR *dp =0;
+
+	bool is_a_directory = isADir( dpath.c_str() );
+
+	if( !is_a_directory)
+	{
+	   return false;
+	}
+
+	if( (dp=opendir(dpath.c_str())) == NULL) {
+		closedir(dp);
+		return false;
+	}
+	 else
+	{
+		closedir(dp);
+		return true;
+	}
+}
+
+void Rompb::setRomPath(string dpath)
+{
+ if( pathExists(dpath) == true)
+ {
+     activeRomPath_m = dpath;
+     getRomList();
+ }
+}
+
+//*********************************************************
+//
+//*********************************************************
+vector<string> Rompb::getRomList( void )
+{
+
+  DIR* dirp;
+  struct dirent* direntp;
+
+  if(activeRomPath_m == "")
+  {
+    activeRomList_vsm.clear();
+	return activeRomList_vsm;
+  }
+
+  if( !pathExists( activeRomPath_m) )
+  {
+	cout << "Directory '%s' not found ...";
+    activeRomList_vsm.clear();
+    return activeRomList_vsm;
+  }
+
+  dirp = opendir( activeRomPath_m.c_str() );
+  if( dirp != NULL )
+  {
+	 for(;;)
+	 {
+		direntp = readdir( dirp );
+		if( direntp == NULL )
+		  break;
+
+   	    string tmp = direntp->d_name;
+
+		if( strcmp( direntp->d_name, ".") == 0)
+		{
+	      continue;
+		}
+
+		if( strcmp( direntp->d_name,"..") == 0)
+		{
+		  continue;
+		}
+        cout << tmp;
+		string extension = tmp.substr(tmp.find_last_of(".") +1);
+        if( extensionIsValid( extension ) == true)
+		{
+	      fprintf(stderr,"ROM -> %s\n", direntp->d_name);
+	      activeRomList_vsm.push_back(direntp->d_name);
+	    }
+	 }
+  }
+  else
+  {
+	fprintf(stderr,"dirp is NULL ...\n");
+  }
+
+ // sort list here .
+ fprintf(stderr,"number of files %d\n", activeRomList_vsm.size() );
+ return activeRomList_vsm;
+}
+
+
+
+
+
+//*********************************************************
+//
+//*********************************************************
+const char *Rompb::getRomNext(void)
+{
+    fprintf(stderr,"getRomNext: %d\n", romType_m);
+    static char romName[128];
+
+    memset(romName,0,128);
+
+	if( activeRomList_vsm.size() == 0)
+	{
+	   fprintf(stderr,"getRomNext: error no games in vecList\n");
+	   return "";
+	}
+
+    if(++activeRomIndex_m >= activeRomList_vsm.size())
+    	activeRomIndex_m = 0;
+
+    if( activeRomList_vsm.size() == 1)
+    	activeRomIndex_m = 0;
+
+    if( activeRomIndex_m == activeRomList_vsm.size())
+    	activeRomIndex_m = 0;
+
+
+    activeRom_m = activeRomList_vsm[activeRomIndex_m];
+    string baseDir = activeRomPath_m + "/" + activeRomList_vsm[activeRomIndex_m];
+
+    sprintf(romName,"%s",baseDir.c_str());
+
+    fprintf(stderr,"loading: %d/%d '%s'\n", activeRomIndex_m, activeRomList_vsm.size(), baseDir.c_str() );
+    return romName;
+}
+
+//*********************************************************
+//
+//*********************************************************
+const char *Rompb::getRomPrev(void)
+{
+    fprintf(stderr,"getRomPrev: %d\n", romType_m);
+    static char romName[128];
+
+    memset(romName,0,128);
+
+	if( activeRomList_vsm.size() == 0)
+	{
+	   fprintf(stderr,"getRomPrev: error no games in vecList\n");
+	   return "";
+	}
+
+    if(--activeRomIndex_m < 1)
+    	activeRomIndex_m = 0;
+
+    if( activeRomList_vsm.size() == 1)
+    	activeRomIndex_m = 0;
+
+    activeRom_m = activeRomList_vsm[activeRomIndex_m];
+    string baseDir = activeRomPath_m + "/" + activeRomList_vsm[activeRomIndex_m];
+
+    sprintf(romName,"%s",baseDir.c_str());
+
+    fprintf(stderr,"getRomPrev: %d/%d '%s'\n", activeRomIndex_m, activeRomList_vsm.size(), baseDir.c_str() );
+    return romName;
+}
+
+void Rompb::setActiveRomBad()
+{
+  badRomList_vim.push_back( activeRomIndex_m );
+}
+
+bool Rompb::isBadRom()
+{
+  vector<unsigned int>::iterator i;
+  for( i = badRomList_vim.begin(); i != badRomList_vim.end(); ++i)
+  {
+    if( activeRomIndex_m == *i )
+    	return true;
+  }
+  return false;
+}
+
+string Rompb::getInfoStr()
+{
+  return activeRom_m;
+}
+
+
+//*******************************
+//
+//*******************************
+void Rompb::updateRomList(void)
+{
+  (void) getRomList();
+}
+
+
+void   Rompb::setRomIndex(unsigned int idx)
+{
+   if( idx <= activeRomList_vsm.size() )
+	  activeRomIndex_m = idx;
+   if( activeRomList_vsm.size() )
+   activeRom_m = activeRomList_vsm[activeRomIndex_m];
+}
+
+void Rompb::saveState(void)
+{
+	  unsigned int i = 0;
+	  ofstream cfgFile;
+	  cfgFile.open (cfgFilePath_m.c_str());
+	  cfgFile << activeRomIndex_m << "\n";
+	  cfgFile << activeRom_m << "\n";
+	  for( i = 0; i < activeRomList_vsm.size(); i++)
+	  {
+		 cfgFile << activeRomList_vsm[i] << "\n";
+	  }
+	  cfgFile.close();
+}
+
+void Rompb::loadState(void)
+{
+     ifstream cfgFile;
+     string line;
+
+     cfgFile.open(cfgFilePath_m.c_str());
+
+     // 3
+     // spaceinvaders.gba
+     // blah1.gba
+     // ...
+
+     if (cfgFile.is_open())
+      {
+
+    	getline(cfgFile, line);
+    	//  activeRomIndex_m
+    	getline(cfgFile, line);
+    	//  activeRom_m
+
+        while ( cfgFile.good() )
+        {
+          getline ( cfgFile, line);
+          cout << line << endl;
+          // activeRomList_vsm.push_back(line);
+        }
+        cfgFile.close();
+      }
+
+}
---
+++ src/rompb.h
@@ -0,0 +1,67 @@
+#ifndef _ROM_H_
+#define _ROM_H_
+
+/* Rom is a class that provides file path setting and rom selection, iteration, cycling
+ * It saves repeating the same type of code on multiple emulators.
+ */
+using namespace std;
+#include <vector>
+#include <string>
+#include <cstdio>
+
+
+
+class Rompb {
+
+public:
+
+typedef enum {
+		rom_nes_c = 0,
+		rom_gb_c,
+		rom_gbc_c,
+		rom_gba_c,
+		rom_pce_c,
+		rom_smd_c
+}rom_type_t;
+
+Rompb();
+
+Rompb(rom_type_t rtype);
+
+bool           pathExists(string dpath);
+bool           isADir(string dpath);
+
+vector<string> getRomList(void);
+const char    *getRomNext(void);  // increment up the list
+const char    *getRomPrev(void);  // decrement down the list
+void           updateRomList(void);
+void           sort(void);
+
+void           setRomPath(string path);
+string         getRomPath()       { return activeRomPath_m; };
+string         getActiveRomName() { return activeRom_m; };
+void           setActiveRomBad(void);
+string         getInfoStr();
+int            romCount() { return activeRomList_vsm.size(); };
+bool           isBadRom();
+void           setRomIndex(unsigned int idx);
+bool           extensionIsValid(string ext);
+void           saveState(void);
+void           loadState(void);
+
+private:
+
+ string                  activeRomPath_m;
+ string                  cfgFilePath_m;
+ vector<string>          activeRomList_vsm;
+ vector<string>          extensions_vsm;
+ vector<unsigned int>    badRomList_vim;
+ string                  activeRom_m;
+ rom_type_t              romType_m;
+ unsigned int            activeRomIndex_m;
+};
+
+
+
+
+#endif /* _ROM_H_ */
